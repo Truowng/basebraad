1c8f1e936aca5ef7b346fdc0aad26376dadd0671
Truowng <quockhanhtruong166@gmail.com>
2024-07-17 21:23:16 +0700

    commit

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+// const menuBtn = document.querySelector("#header .menu-button");
+// const closeBtn = document.querySelector(
+//   "#header .header-link-list .close-button"
+// );
+// const menu = document.querySelector("#header .header-link-list");
+
+// menuBtn.addEventListener("click", () => {
+//   menu.classList.add("active");
+// });
+
+// closeBtn.addEventListener("click", () => {
+//   menu.classList.remove("active");
+// });
+
+if (document.querySelector(".marquee-slider")) {
+  const marqueeSlider = new Swiper(".marquee-slider .swiper", {
+    direction: "horizontal",
+    speed: 10000,
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+      delay: 1,
+    },
+    breakpoints: {
+      991: {
+        slidesPerView: 2,
+      },
+    },
+  });
+}
